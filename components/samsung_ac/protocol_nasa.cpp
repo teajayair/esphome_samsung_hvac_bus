bde4a9a27624f233915eea742fadff1df6c91afb
DESKTOP-AT1H8KH\Mithcet <tim.a@aac-ltd.com>
2026-03-31 15:07:08 +0000

    TJA7

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -1000,7 +1000,7 @@ namespace esphome
             {
                 if (debug_log_undefined_messages && should_log_undefined_msg(source, dest, message))
                 {
-                    LOGW("Undefined TJA6 s:%s d:%s %s", source.c_str(), dest.c_str(), message.to_string().c_str());
+                    LOGW("Undefined TJA7 s:%s d:%s %s", source.c_str(), dest.c_str(), message.to_string().c_str());
                 }
                 break;
             }
