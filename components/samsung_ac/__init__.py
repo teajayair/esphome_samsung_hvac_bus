7f31287a194d6b79982a3a6fa9cd697d0bb48b2e
DESKTOP-AT1H8KH\Mithcet <tim.a@aac-ltd.com>
2026-04-09 18:35:59 +0000

    TJA18

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -328,7 +328,6 @@ DEVICE_SCHEMA = cv.Schema(
         cv.Optional(CONF_DEVICE_IN_FLOW_SENSOR): sensor.sensor_schema(
             unit_of_measurement="l/m",
             accuracy_decimals=1,
-            device_class=DEVICE_CLASS_VOLUME_FLOW_RATE,
             state_class=STATE_CLASS_MEASUREMENT,
             icon="mdi:flash",
         ).extend(
